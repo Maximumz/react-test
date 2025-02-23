c246c9fe1f4ead7c170bacecb58574a5bfd83f7a
DinushJay <dinushjay@gmail.com>
2025-02-23 23:55:09 +1100

    Fixed FetchData Component

--- src/shared/components/FetchData.tsx
+++ src/shared/components/FetchData.tsx
@@ -1,18 +1,40 @@
 import React, { useEffect, useState } from 'react';
+interface postData {
+  id: number,
+  body: string,
+  title: string,
+  userId: number
+}
 
 // Fix this component so it properly fetches data and handles loading state.
 const FetchData = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<postData | null>(null); 
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts/1')
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to Fetch Data'); // Handle HTTP errors.
+        }
+        return response.json();
+      })
       .then((result) => {
+        //console.log(result);
         setData(result);
+        setLoading(false);
+      })
+      .catch((error) => {
+        // Set error message.
+        setError(error.message);
         setLoading(false);
       });
   }, []);
+
+  if (error) {
+    return <p>Error: {error}</p>; // Display error message
+  }
 
   if (loading) {
     return <p>Loading...</p>;
@@ -21,7 +43,7 @@ const FetchData = () => {
   return (
     <div>
       <h3>Data:</h3>
-      <p>{data.title}</p>
+      <p>{data ? data.title : 'No data available'}</p>
     </div>
   );
 };
